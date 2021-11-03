35df99c49a65b3f87dcabeb8d34e8a276772d258
akrahdan <akrahdan@gmail.com>
2021-11-02 22:27:39 -0500

    initial commit

---
+++ clozetask/retrieval.py
@@ -0,0 +1,54 @@
+import os
+from typing import Optional
+
+from clozetask.core import Task
+from clozetask.task import SemevalTask
+from clozetask.utils.io import read_json
+
+TASK_DICT = {
+    "sem_mclass": SemevalTask
+}
+
+def get_task_class(task_name: str):
+    task_class = TASK_DICT[task_name]
+    assert issubclass(task_class, Task)
+    return task_class
+
+
+def create_task_from_config(config: dict, base_path: Optional[str] = None, verbose: bool = False):
+    """Create task instance from task config.
+    Args:
+        config (Dict): task config map.
+        base_path (str): if the path is not absolute, path is assumed to be relative to base_path.
+        verbose (bool): True if task config should be printed during task creation.
+    Returns:
+        Task instance.
+    """
+    task_class = get_task_class(config["task"])
+    for k in config["paths"].keys():
+        path = config["paths"][k]
+        # TODO: Refactor paths  (issue #1180)
+        if isinstance(path, str) and not os.path.isabs(path):
+            assert base_path
+            config["paths"][k] = os.path.join(base_path, path)
+    task_kwargs = config.get("kwargs", {})
+    if verbose:
+        print(task_class.__name__)
+        for k, v in config["paths"].items():
+            print(f"  [{k}]: {v}")
+    # noinspection PyArgumentList
+    return task_class(name=config["name"], path_dict=config["paths"], **task_kwargs)
+
+
+def create_task_from_config_path(config_path: str, verbose: bool = False):
+    """Creates task instance from task config filepath.
+    Args:
+        config_path (str): config filepath.
+        verbose (bool): True if task config should be printed during task creation.
+    Returns:
+        Task instance.
+    """
+    return create_task_from_config(
+        read_json(config_path), base_path=os.path.split(config_path)[0], verbose=verbose,
+    )
+
---
+++ clozetask/task.py
@@ -0,0 +1,95 @@
+from dataclasses import dataclass
+from typing import List
+import numpy as np
+import torch
+from clozetask.lib.feature_template import single_sentence_featurize, labels_to_bimap
+from clozetask.utils.io import read_csv
+from .core import (
+    BaseExample,
+    BaseTokenizedExample,
+    BaseDataRow,
+    BatchMixin,
+    SuperGlueMixin,
+    Task,
+    TaskTypes
+)
+
+@dataclass
+class Example(BaseExample):
+    guid: str
+    passage_text: str
+    label: str
+    
+    def tokenize(self, tokenizer):
+        return super().tokenize(tokenizer)
+
+class TokenizedExample(BaseTokenizedExample):
+    guid: str
+    passage_text: List
+    label_id: int
+
+    def featurize(self, tokenizer, feat_spec):
+        return single_sentence_featurize(
+            guid=self.guid,
+            input_tokens=self.passage_text,
+            label_id=self.label_id,
+            tokenizer=tokenizer,
+            feat_spec=feat_spec,
+            data_row_class=DataRow
+        )
+
+@dataclass
+class DataRow(BaseDataRow):
+    guid: str
+    input_ids: np.ndarray
+    input_mask: np.ndarray
+    segment_ids: np.ndarray
+    label_id: int
+    tokens: list
+
+@dataclass
+class Batch(BatchMixin):
+    input_ids: torch.LongTensor
+    input_mask: torch.LongTensor
+    segment_ids: torch.LongTensor
+    label_id: torch.LongTensor
+    tokens: list
+
+
+class SemevalTask(SuperGlueMixin, Task):
+    Example = Example
+    TokenizedExample = Example
+    DataRow = DataRow
+    Batch = Batch
+    Task_Type = TaskTypes.MULTI_LABEL_SPAN_CLASSIFICATION
+    LABELS = ["0", "1", "2"]
+    LABEL_TO_ID, ID_TO_LABEL = labels_to_bimap(LABELS)
+
+    def get_train_examples(self):
+        
+        return self._create_examples(lines=read_csv(self.train_path, self.label_path), set_type="train")
+
+    def get_val_examples(self):
+        return self._create_examples(lines=read_csv(self.val_path, self.label_path), set_type="val")
+
+    def get_test_examples(self):
+        return self._create_examples(lines=read_csv(self.test_path, self.label_path), set_type="test")
+    
+    @classmethod
+    def _create_examples(cls, lines, set_type):
+        examples = []
+        for (i, line) in enumerate(lines):
+            examples.append(
+                Example(
+                    # NOTE: get_glue_preds() is dependent on this guid format.
+                    guid="%s-%s" % (set_type, i),
+                    passage_text=line[0],
+                    label=line[1] if set_type != "test" else cls.LABELS[-1],
+                )
+            )
+        return examples
+    
+    @staticmethod
+    def super_glue_format_preds(pred_dict):
+        return pred_dict["preds"]
+
